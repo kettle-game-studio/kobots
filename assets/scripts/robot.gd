d4f01362738b135d1e654b58415b79604a57399b
SID37 <1999login@mail.ru>
2023-11-18 19:22:04 +0300

    Add terminals

---
+++ assets/scripts/robot.gd
@@ -0,0 +1,6 @@
+# @tool
+extends Node3D
+class_name TestRobot
+
+@export var camera: Camera3D
+@export var subViewport: SubViewport
---
+++ assets/scripts/terminal/terminal.gd
@@ -0,0 +1,24 @@
+@tool
+extends Node3D
+class_name Terminal
+
+@export var robot: TestRobot
+@export var monitor: MeshInstance3D
+
+func _update_texture():
+	if monitor == null or robot == null:
+		return
+	# monitor.set_instance_shader_parameter("camera_texture", robot.subViewport.get_texture())
+	
+	var material = monitor.get_surface_override_material(0) as ShaderMaterial
+	material = material.duplicate()
+	material.set_shader_parameter("camera_texture", robot.subViewport.get_texture())
+	monitor.set_surface_override_material(0, material)
+	# monitor.set_instance_shader_parameter("test", 0.5)
+
+func _ready():
+	_update_texture()
+
+func _process(delta):
+	if not Engine.is_editor_hint(): return
+	_update_texture()
