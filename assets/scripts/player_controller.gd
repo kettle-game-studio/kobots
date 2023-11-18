0f8bdd5799b2260789b2407756accff703f17677
Tetrergeru <ArutunoffOleg@yandex.ru>
2023-11-18 16:34:55 +0100

    Add box pushing

--- assets/scripts/player_controller.gd
+++ assets/scripts/player_controller.gd
@@ -1,9 +1,20 @@
 extends Node
 
-@export var player: Node3D
+@export var player: CharacterBody3D
 @export var camera: Camera3D
 
 @export var camera_speed: float = 0.01
+@export var walk_speed: float = 5
+@export var push_force: float = 100
+
+func _physics_process(delta: float):
+	walk(delta)
+	for index in player.get_slide_collision_count():
+		var collision = player.get_slide_collision(index)
+		var collider = collision.get_collider()
+		if collider is RigidBody3D:
+			var vector = (player.get_position() - collider.get_position()).normalized()
+			collider.apply_central_force(-vector * push_force)
 
 func _input(event: InputEvent):
 	if event is InputEventMouseMotion:
@@ -18,4 +29,15 @@ func _input(event: InputEvent):
 func rotate_camera(vector: Vector2):
 	camera.rotation.x = clamp(camera.rotation.x - vector.y * camera_speed, -PI/2, PI/2)
 	player.rotate_y(-vector.x * camera_speed)
+
+func walk(delta: float):
+	var aim = player.get_global_transform().basis
+	
+	var forward = Input.get_axis("Down", "Up") * -aim.z
+	var right = Input.get_axis("Left", "Right") * aim.x
+	
+	var direction: Vector3 = (forward + right).normalized()
+	
+	player.velocity = direction * walk_speed
+	player.move_and_slide()
 
