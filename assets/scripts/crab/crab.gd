abee6d74253a736227210c87701519e1272073d6
SID37 <1999login@mail.ru>
2023-11-19 23:36:54 +0300

    Add crab scripts

--- assets/scripts/crab/crab.gd
+++ assets/scripts/crab/crab.gd
@@ -1,11 +1,28 @@
-extends Node
+@tool
+extends Node3D
+
+@export var color: Color
+@export var enable_laser: bool
+@export var meshes: Array[MeshInstance3D]
+@export var light_meshes: Array[MeshInstance3D]
 
 
-# Called when the node enters the scene tree for the first time.
+func _update_parameters():
+	for mesh in light_meshes + meshes:
+		var material = mesh.get_surface_override_material(0) as ShaderMaterial
+		material = material.duplicate()
+		material.set_shader_parameter("color", color)
+		mesh.set_surface_override_material(0, material)
+	for mesh in light_meshes:
+		var material = mesh.get_surface_override_material(0) as ShaderMaterial
+		material.set_shader_parameter("enable", enable_laser)
+
+
 func _ready():
-	pass # Replace with function body.
+	_update_parameters()
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	if Engine.is_editor_hint():
+		_update_parameters()
 	pass
