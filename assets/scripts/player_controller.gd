d7436ea5aa4c133aa5a8478a716d8761c70db96e
Tetrergeru <ArutunoffOleg@yandex.ru>
2023-11-22 19:12:41 +0100

    Experiment with mouse sencetivity

--- assets/scripts/player_controller.gd
+++ assets/scripts/player_controller.gd
@@ -81,11 +81,13 @@ func _input(event: InputEvent):
 		main_ui.clear_text()
 
 func rotate_camera(vector: Vector2):
+	var window_size = DisplayServer.window_get_size()
 	self.is_rotationg = true
 	self.rotating_timer = 0.02
-	var velocity = vector.y * camera_speed
+	var cam_speed = camera_speed / (window_size.x / 1920.0)
+	var velocity = vector.y * cam_speed
 	camera.rotation.x = clamp(camera.rotation.x - velocity, -PI/2, PI/2)
-	player.rotate_y(-vector.x * camera_speed)
+	player.rotate_y(-vector.x * cam_speed)
 
 func walk(delta: float):
 	if !enabled:
