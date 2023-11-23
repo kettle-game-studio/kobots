9c30c6eb7ceb56f9a328c1fc4b4df71477478e10
SID37 <1999login@mail.ru>
2023-11-23 14:08:41 +0300

    Blink laser emitters

--- assets/scripts/laser_absorber.gd
+++ assets/scripts/laser_absorber.gd
@@ -13,15 +13,18 @@ func _ready():
 	for mesh in meshes:
 		var material = mesh.get_surface_override_material(0) as ShaderMaterial
 		material = material.duplicate()
-		material.set_shader_parameter("enable", false)
+		material.set_shader_parameter("enable", true)
 		mesh.set_surface_override_material(0, material)
 		materials.push_back(material)
+	_disable_materials()
 
 
 func enable_laser(color: Color):
 	pressed.emit(self)
 	for material in materials:
-		material.set_shader_parameter("enable", true)
+		material.set_shader_parameter("emission_k", 1)
+		material.set_shader_parameter("emission_period", 7)
+		material.set_shader_parameter("emission_amplitude", 0.4)
 		material.set_shader_parameter("color", color)
 
 
@@ -29,9 +32,13 @@ func disable_laser():
 	if freezed:
 		return
 	unpressed.emit(self)
-	for material in materials:
-		material.set_shader_parameter("enable", false)
+	_disable_materials()
 
+func _disable_materials():
+	for material in materials:
+		material.set_shader_parameter("emission_k", 0.15)
+		material.set_shader_parameter("emission_period", 2)
+		material.set_shader_parameter("emission_amplitude", 0.1)
 
 func freeze_as_activated():
 	freezed = true
