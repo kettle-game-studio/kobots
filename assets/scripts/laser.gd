a4d3a53fb9af702791b2700b10be31672f474702
SID37 <1999login@mail.ru>
2023-11-27 11:28:46 +0300

    Add laser shader

--- assets/scripts/laser.gd
+++ assets/scripts/laser.gd
@@ -1,19 +1,28 @@
 extends RayCast3D
 class_name Laser
 
-@onready var beam_mesh: MeshInstance3D = $BeamMesh
+@export var beam_meshes: Array[MeshInstance3D]
+
+@onready var beam_root: Node3D = $BeamRoot
+@onready var beam_particles: GPUParticles3D = $EndParticles
 @onready var particles = $EndParticles
 @onready var emit_audio = $EmitAudioStream
 @onready var collision_audio = $CollisionAudioStream
 var laser_target: Object = null
-var laser_material: BaseMaterial3D
+var laser_material: ShaderMaterial
+var partcle_material: StandardMaterial3D
 var color: Color = Color(1, 1, 1)
 
 func _ready():
-	var material = beam_mesh.get_surface_override_material(0) as BaseMaterial3D
+	var material = beam_meshes[0].get_surface_override_material(0) as ShaderMaterial
 	laser_material = material.duplicate(true)
-	beam_mesh.mesh = beam_mesh.mesh.duplicate()
-	beam_mesh.set_surface_override_material(0, laser_material)
+	for m in beam_meshes:
+		m.mesh = m.mesh.duplicate()
+		m.set_surface_override_material(0, laser_material)
+	var p_material = beam_particles.material_override as StandardMaterial3D
+	partcle_material = p_material.duplicate(true)
+	beam_particles.material_override = partcle_material
+
 	if self.visible:
 		self.emit_audio.play()
 		self.collision_audio.stop()
@@ -43,8 +52,7 @@ func _process(delta):
 		collider.enable_laser(color)
 
 func fire_at_cast_point(cast_point: float):
-	beam_mesh.mesh.height = abs(cast_point)
-	beam_mesh.position.z = cast_point / 2
+	beam_root.scale.z = abs(cast_point) / 100
 	particles.position.z = cast_point
 	collision_audio.position.z = cast_point
 
@@ -65,7 +73,8 @@ func enable():
 	self.visible = true;
 
 func set_color(color: Color):
-	laser_material.emission = color
-	laser_material.albedo_color = color
+	laser_material.set_shader_parameter("color", color)
+	partcle_material.emission = color
+	partcle_material.albedo_color = color
 	self.color = color
 	pass
